e6b75cf573bd8d3ac87b7f14a8d99db2420b19eb
sahilkhan-t <TECHEFFICIO_Sahil.Khan2@maruti.co.in>
2026-05-25 10:48:14 +0530

    validation added using Zod

--- src/contollers/registerUserContoller.js
+++ src/contollers/registerUserContoller.js
@@ -1,42 +1,54 @@
 
 const prisma = require('../config/prisma');
 const { encrypt } = require('../utils/encrypt');
-const registerUser = async (req,res)=>{
+const { registerSchema } = require('../validators/authValidator');
+const registerUser = async (req, res) => {
 
-    console.log('reqBody',req.body)
+  console.log('reqBody', req.body)
 
-    try{
+  try {
 
-      const {name,email,password} = req.body;
-      
-      if(!name || !email || !password){
-        return res.status(400).json({
-            message:"All Fileds are required",
-            success: false
-        })
-      }
+    const validateSchema = registerSchema(req.body);
 
-      const hashedPassword = await encrypt(password);
+    console.log('vaidateSchema', validateSchema)
 
-      const user = await prisma.user.create({
-        data:{
-            name,email,password:hashedPassword
-        }
-      })
+    const { name, email, password } = req.body;
 
-      res.status(201).json({
-        message: "User registered successfully",
-        success: true,
-        user
-      })
-
-    }catch(error){
-      res.status(500).json({
-        message:"Internal Server Error",
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All Fileds are required",
         success: false
       })
     }
+
+    const hashedPassword = await encrypt(password);
+
+    const user = await prisma.user.create({
+      data: {
+        name, email, password: hashedPassword
+      }
+    })
+
+    res.status(201).json({
+      message: "User registered successfully",
+      success: true,
+      user
+    })
+
+  } catch (error) {
+    if (error.name === 'ZodError') {
+      return res.status(400).json({
+        success: false,
+        errors: error.issues
+      });
+    }
+
+    res.status(500).json({
+      message: "Internal Server Error",
+      success: false
+    })
+  }
 }
-module.exports={
-    registerUser
+module.exports = {
+  registerUser
 }
---
+++ src/validators/authValidator.js
@@ -0,0 +1,11 @@
+const {z} = require('zod');
+
+const registerSchema = z.object({
+    name:z.string().min(3),
+    email:z.string().email(),
+    password:z.string().min(6)
+})
+
+module.exports = {
+    registerSchema
+}
